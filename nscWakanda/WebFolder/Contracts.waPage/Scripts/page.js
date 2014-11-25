982f395c2da24c4c9e50866af47147af1b457601
Mike Beatty <mikebeatty.us@gmail.com>
2014-11-25 11:54:18 -0500

    Update page.js

--- nscWakanda/WebFolder/Contracts.waPage/Scripts/page.js
+++ nscWakanda/WebFolder/Contracts.waPage/Scripts/page.js
@@ -1,1 +1,125 @@
-﻿//Welsh Harris (14:21):/** * @fileOverview Code to init everything for the main app page * @author Welsh Harris * @created 11/24/2014 *//** @namespace */var Wap = Wap || {};WAF.onAfterInit = function() {    "use strict";    Wap.page = (function() {        //module API        //=================================================================================================       var viewsGrid = $$("dataGrid1"),           viewComp = $$("component1");        //setup views        viewsArr = [           {name: "contract", path: "/Contracts.waPage/views/view1.waComponent"}           ];                  sources.viewsArr.sync();//        views = [//            {name: "auth", description: "@Show Portal", path: "/portal.waPage/views/auth.waComponent"},//        ];        /**         * load a view into the main view component         * @param {string} viewName - name of the view as setup in the viewsArr         * @param {object} [userData] - data to send to the component when it loads into the view         */                  function displaySelectedRecord(){                var contractID = sources.contractArr.Contract;		sources.contracts.query('ContractID == :1',contractID);//		sources.rMA_OnSite.query('RMA_ID == :1',contractID);				viewComp.displayContractDetail();			        };                              		function goToView(viewName) {           var view;           //get info about the view we want to go to           view = _.find(viewsArr, function(view) {return view.name === viewName;});           //load view into the main view component           viewComp.removeComponent();           viewComp.loadComponent(               {path: view.path}           );       }        /**         * Handle logging out         */        function onAfterLogout() {            goToView("auth");        }        //on load        //=================================================================================================        function onLoad() {                      //clicking tabs loads the appropriate view//            WAF.addListener(messagesTab, "click", function() {//                goToView("messages");//            });       WAF.addListener(viewsGrid, "onRowClick", function() {//           goToView(sources.viewsArr.name);		displaySelectedRecord();       });            //        Wap.auth.getCurrUserName()//		console.log('getname');		var userName = WAF.directory.currentUser().fullName;//		console.log(userName);//		sources.web_Access.query('WebLogOn == :1',userName);//		console.log(sources.web_Access.length);//		var vcompanyID = sources.web_Access.CompanyID;//		console.log(vcompanyID);				var vcompanyID = '73718';		//		sources.rMA_OnSite.query('SSP_ID == :1',sources.web_Access.CompanyID);//		sources.rMA_OnSite.query('SSP_ID == :1',vSSPID);		sources.contracts.wak_getContractArr(vcompanyID,{				//consider using WakAPI as standard table		onSuccess: function(event) {		contractArr = JSON.parse(event.result);		sources.contractArr.sync();	}			});                }                   goToView("contract");        //public API        //=================================================================================================        return {            goToView: goToView,            onLoad: onLoad        };    }());    Wap.page.onLoad();};
+//Welsh Harris (14:21):
+/**
+ * @fileOverview Code to init everything for the main app page
+ * @author Welsh Harris
+ * @created 11/24/2014
+ */
+
+/** @namespace */
+var Wap = Wap || {};
+
+
+WAF.onAfterInit = function() {
+    "use strict";
+
+    Wap.page = (function() {
+
+//        //module API
+        //=================================================================================================
+       var viewsGrid = $$("dataGrid1"),
+           viewComp = $$("componentDetail"),
+           views;
+
+        //setup views
+        viewsArr = [
+           {name: "contract", path: "/Contracts.waPage/views/view1.waComponent"}
+           ];
+           
+       sources.viewsArr.sync();
+        views = [
+            {name: "auth", description: "@Show Portal", path: "/portal.waPage/views/auth.waComponent"},
+        ];
+
+        /**
+         * load a view into the main view component
+         * @param {string} viewName - name of the view as setup in the viewsArr
+         * @param {object} [userData] - data to send to the component when it loads into the view
+         */
+         
+         function displaySelectedRecord(){
+        
+        var contractID = sources.contractArr.Contract;
+		sources.contracts.query('ContractID == :1',contractID);
+//		sources.rMA_OnSite.query('RMA_ID == :1',contractID);
+		
+		viewComp.displayContractDetail();
+	
+		
+        };
+         
+         
+         
+   		function goToView(viewName) {
+           var view;
+
+           //get info about the view we want to go to
+           view = _.find(viewsArr, function(view) {return view.name === viewName;});
+
+           //load view into the main view component
+           viewComp.removeComponent();
+           viewComp.loadComponent(
+               {path: view.path}
+           );
+       }
+
+        /**
+         * Handle logging out
+         */
+        function onAfterLogout() {
+            goToView("auth");
+        }
+
+        //on load
+        //=================================================================================================
+        function onLoad() {
+          
+            //clicking tabs loads the appropriate view
+//            WAF.addListener(messagesTab, "click", function() {
+//                goToView("messages");
+//            });
+
+       WAF.addListener(viewsGrid, "onRowClick", function() {
+//           goToView(sources.viewsArr.name);
+		displaySelectedRecord();
+       });
+            
+//        Wap.auth.getCurrUserName()
+//		console.log('getname');
+
+		var userName = WAF.directory.currentUser().fullName;
+		console.log(userName);
+		sources.web_Access.query('WebLogOn == :1',userName);
+		console.log(sources.web_Access.length);
+		var vcompanyID = sources.web_Access.CompanyID;
+		console.log(vcompanyID);
+		var vcompanyID = '73718';
+		
+//		sources.rMA_OnSite.query('SSP_ID == :1',sources.web_Access.CompanyID);
+//		sources.rMA_OnSite.query('SSP_ID == :1',vSSPID);
+
+		sources.contracts.wak_getContractArr(vcompanyID,{				//consider using WakAPI as standard table
+		onSuccess: function(event) {
+		contractArr = JSON.parse(event.result);
+		sources.contractArr.sync();
+	}
+		
+	});
+        
+        }
+        
+        
+   goToView("contract");
+
+        //public API
+        //=================================================================================================
+        return {
+            goToView: goToView,
+            onLoad: onLoad
+        };
+
+    }());
+
+    Wap.page.onLoad();
+};
+
+
